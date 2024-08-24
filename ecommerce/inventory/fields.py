3814d4609e77ff2cb046a4ae9d4fc0a4e3b51f89
slonn <VanoGalen@yandex.ru>
2024-08-24 22:43:06 +0300

    custom fields

--- ecommerce/inventory/fields.py
+++ ecommerce/inventory/fields.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 
 
@@ -8,3 +9,43 @@ class ProductIDField(models.CharField):
         kwargs['max_length'] = 255
         kwargs['unique'] = True
         super().__init__(*args, **kwargs)
+
+
+class OrderField(models.PositiveIntegerField):
+    def pre_save(self, model_instance, add):
+
+        if getattr(model_instance, self.attname) is None:
+
+            try:
+                obj = self.model.objects.latest(self.attname)
+                print(obj)
+                value = obj.order + 1
+
+            except ObjectDoesNotExist:
+                value = 1
+
+            setattr(model_instance, self.attname, value)
+
+            return value
+
+        else:
+            return super().pre_save(model_instance, add)
+
+
+class HexColorField(models.CharField):
+    description = 'A hexadecimal color code'
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 6
+        kwargs['null'] = True
+        kwargs['blank'] = True
+        super().__init__(*args, **kwargs)
+
+    def db_type(self, connection):
+        return 'INTEGER(3)'
+
+    def get_prep_value(self, value):
+        return value and int(value, 16)
+
+    def from_db_value(self, value, expression, connection):
+        return value and format(value, 'X')
--- ecommerce/inventory/models.py
+++ ecommerce/inventory/models.py
@@ -1,11 +1,16 @@
 from django.db import models
-from .fields import ProductIDField
+from .fields import ProductIDField, OrderField, HexColorField
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     product_id_field = ProductIDField()
+    order = OrderField()
+    colour = HexColorField()
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+
+'#ca0e68'

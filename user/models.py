4a9867be88b8c842a819d8d6a031a55117ffa0b1
slonn <VanoGalen@yandex.ru>
2024-08-25 17:56:23 +0300

    proxy model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class PersonManagerInActive(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=False)
+
+
+class PersonManagerActive(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
+
+class Person(User):
+    inactive = PersonManagerInActive()
+    active = PersonManagerActive()
+
+    class Meta:
+        proxy = True
+        ordering = ['first_name', ]
+
+    @classmethod
+    def count_all(cls, status=None):
+        return cls.objects.filter(is_active=True).count()
+
+    @property
+    def check_active(self):
+        if self.is_active:
+            return 'u are active'
+
+        return 'u are not active'
+
+    def __str__(self):
+        return self.first_name

d16177b46eb00fb8dc8cacfbe3918f6f81c7586c
slonn <VanoGalen@yandex.ru>
2024-08-25 16:40:32 +0300

    user intermideate

--- ecommerce/inventory/signals.py
+++
@@ -1,9 +0,0 @@
-from django.dispatch import receiver
-from django.db.models.signals import post_save
-from .models import Category
-
-
-@receiver(post_save, sender=Category)
-def example_receiver(sender, instance, **kwargs):
-    print(kwargs)
-    print('instance is saved')
